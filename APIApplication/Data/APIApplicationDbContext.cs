9dc9d6e497cf6b9d9afe7b2fdec37e33c3690b25
dilu <dilushika.chithranagani@gmail.com>
2022-08-19 23:49:27 +0530

    Add project files.

---
+++ APIApplication/Data/APIApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APIApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIApplication.Data
+{
+    public class APIApplicationDbContext : DbContext
+    {
+        // To create Constructor
+        public APIApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
